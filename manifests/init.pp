0a765f292aeae86f2d7d69fb5af8e64717faa534
Henrik <henrik@haf.se>
2012-07-24 01:59:09 +0200

    without files

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@
 class logstash($version = '1.1.1') {
 
   file { "/opt/logstash-${version}-monolithic.jar":
-    source => "puppet:///modules/logstash/files/logstash-${version}-monolithic.jar",
+    source => "puppet:///modules/logstash/logstash-${version}-monolithic.jar",
     ensure => present
   }
   
