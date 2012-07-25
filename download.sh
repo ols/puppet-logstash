47a76e056bd12d37754bc6cbe9020b7f370cc4ff
Henrik <henrik@haf.se>
2012-07-25 17:24:16 +0200

    download file improvement

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-read -p "Type LogStash version to download [1.1.1]: " -e 1.1.1 -i ls_version
+default=1.1.1
+read -p "Type LogStash version to download [1.1.1]: " -e ls_version
+ls_version=${ls_version:-$default}
 ls_archive="logstash-$ls_version-monolithic.jar"
 ls_source="http://semicomplete.com/files/logstash"
 curl -o files/$ls_archive $ls_source/$ls_archive
