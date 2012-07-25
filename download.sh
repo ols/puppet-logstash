75cde55cce7847f6e3799d9a0591b5d7db555082
Henrik <henrik@haf.se>
2012-07-25 17:12:55 +0200

    adding name to download file

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Type version to download [1.1.1]: " -e 1.1.1 -i ls_version
+read -p "Type LogStash version to download [1.1.1]: " -e 1.1.1 -i ls_version
 ls_archive="logstash-$ls_version-monolithic.jar"
 ls_source="http://semicomplete.com/files/logstash"
 curl -o files/$ls_archive $ls_source/$ls_archive
